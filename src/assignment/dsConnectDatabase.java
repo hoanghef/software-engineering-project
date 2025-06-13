6d28efa520dd935dd6253cf95e3c08dc9054d126
hoanghef <23010033@st.phenikaa-uni.edu.vn>
2025-06-13 11:02:02 +0700

    update lại giao diện

--- src/assignment/dsConnectDatabase.java
+++ src/assignment/dsConnectDatabase.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package assignment;
 
 import java.sql.Connection;
@@ -5,7 +10,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 
 public class dsConnectDatabase {
     String connectionString = "jdbc:sqlserver://DESKTOP-G7ITISB\\SQLEXPRESS:1433;" 
